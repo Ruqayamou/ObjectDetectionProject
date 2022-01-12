b4eab082c343e729d955c34862c1fb04cda3c62c
Ruqayamou <92495478+Ruqayamou@users.noreply.github.com>
2022-01-11 19:00:21 -0800

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,45 @@
+img1 = "";
+objects = [];
+status = "";
+
+function preload(){
+    img1 = loadImage("bedroom.PNG")
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded!");
+    status = true;
+    objectDetector.detect(img1, gotresults);
+}
+
+function gotresults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img1, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects detected";
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
